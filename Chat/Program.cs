1a5b9b8649e8dc37a5d4c1be8bc583ec6eeb744c
Tillus92 <61047201+Tillus92@users.noreply.github.com>
2021-03-03 12:56:42 +0100

    Projektdateien hinzufügen.

---
+++ Chat/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat
+{
+    class Program //Kaffee
+    {
+        static void Main(string[] args)
+        {
+            Mann mann = new Mann();
+            Frau frau = new Frau();
+            mann.name = "Horst";
+            frau.name = "Susann";
+            frau.istInteressiert = true;
+
+            //Das Bild des Mannes von der Frau entspricht dem Object frau. 
+  
+            Console.WriteLine(mann.AnsprechenDerFrau(frau));
+            Console.WriteLine(frau.antwortFrau(mann));
+            Console.ReadLine();
+        
+
+
+        }
+    }
+    class Mann
+    {
+        public string name; 
+        //
+        public Frau kennt;
+        
+        public string AnsprechenDerFrau(Frau frau)
+        {
+            return frau.istInteressiert ? "Mann: Hi was geht, ich bin " + name + " darf ich Sie auf einen Kaffe einladen?"  : "Mann sagt nichts und verlässt das Kaffee";
+        }
+    }
+    class Frau
+    {
+        //Frau erhält Referenz des Typen Mann
+        public Mann kennt;
+        public string name;
+        public bool istInteressiert;
+
+        public string antwortFrau(Mann mann)
+        {
+            return istInteressiert ? "Frau: Hallo " + mann.name + " mein Name ist "  + name + " gerne können wenn wir einen Kaffe trinken, wenn du zahlst!" : "Kauft sich einen eigenen Kaffe";
+        }
+    }
+
+}
---
+++ Chat_zwei/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_zwei
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpieleKonsole playstation = new SpieleKonsole();
+            Reciever dolby = new Reciever();
+            Screen bildschirm = new Screen(dolby, playstation);
+            playstation.active = false;
+            bildschirm.active = true;
+            dolby.active = false;
+            //Console.WriteLine("Screen Kanal: " + Screen.kanal);
+            //Screen.kanal = 2;
+            // Console.WriteLine("Screen Kanal: " +  Screen.kanal);
+            Console.WriteLine(bildschirm.SwitchChannel(dolby, playstation));        
+            Console.ReadLine();
+
+
+        }
+    }
+    class Screen
+    {
+        public bool active;
+       
+        public Reciever kenntReciever;
+        public SpieleKonsole kenntKonsole;
+        public static int kanal = 1;
+        
+        public Screen(Reciever kenntReciever, SpieleKonsole kenntKonsole)
+        {
+            this.kenntReciever = kenntReciever;
+            this.kenntKonsole = kenntKonsole;
+        }
+        public string SwitchChannel(Reciever r, SpieleKonsole s)
+        {
+            if (active && r.active && kanal == 1)
+            {
+                return this.ToString() + " " + r.ToString() + "Kanal" + kanal;
+            } else if (active && r.active == false && s.active)
+            {
+                kanal = 2;
+                if (kanal == 2)
+                {
+                    return this.ToString() + " " + s.ToString() + " Kanal: " + kanal;
+                }
+            } else if (active && r.active == false && s.active == false)
+            {
+                return this.ToString() + " Kein aktives Signal im HDMI ";
+            } 
+           return " ";
+        }
+        public override string ToString()
+        {
+            return "Der Screen ist AN!";
+        }
+       
+    }
+    class Reciever
+    {
+        public bool active;
+       
+        
+        public override string ToString()
+        {
+            return "Receiver belegt gerade den HDMI ausgang.";
+        }
+    }
+    class SpieleKonsole
+    {
+        public bool active;
+      
+        
+        public override string ToString()
+        {
+            return "Konsole belegt gerade den HDMI ausgang.";
+        }
+    }
+}
